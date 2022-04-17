54e11a9f539e743ccd00d62e86d6ed6abc6daaea
ernman37 <Rion.duckworth@gmail.com>
2022-04-17 02:42:02 -0600

    add shared memory to analyzer, scanner, trader

--- src/CoinsScanner.py
+++ src/CoinsScanner.py
@@ -6,19 +6,25 @@
     Description: Scans a list of coins data for analysis
 '''
 from CoinData import CoinData
-import time
+import time, logging, queue, sys
 
 class CoinsScanner:
-    def __init__(self, coins, timeFrame='1m'):
+    def __init__(self, coins, queue: queue.Queue, timeFrame='1m'):
+        self.log = logging.getLogger()
+        self.log.error("Setting up Scanner")
         self.tickerList = list(coins)
+        self.queue = queue
         self.coins = dict()
         for coin in coins:
             self.coins[coin] = CoinData(coin, timeFrame=timeFrame)
+            self.queue.put(coin)
         self.currentTime = self.timeInMinutes()
 
     def scanForever(self):
+        self.log.error("Begginning To Scan Forever")
         while True:
             if self.isNewCoin():
+                self.log.error("Fetching new coins")
                 self.fetchAllNewCandles()
             else:
                 time.sleep(1)
@@ -34,6 +40,10 @@ class CoinsScanner:
         for coin in self.tickerList:
             while not self.coins[coin].addNewCandle():
                 time.sleep(2) # Sleep for a little if we fetched last candle
+            self.log.error("Added new candle to " + coin)
+            while self.queue.full():
+                time.sleep(1)
+            self.queue.put(coin)
 
     def timeInMinutes(self):
             secs = time.time()
