166e5e4f02b3857553165c71d8eacdd085432a02
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2022-04-28 02:16:49 -0600

    Far more stable graph loop

--- src/CoinUI.py
+++ src/CoinUI.py
@@ -1,3 +1,4 @@
+from ast import Global
 from turtle import color
 import dearpygui.dearpygui as dpg
 import time
@@ -9,71 +10,94 @@ from CryptoBot import CryptoBot
 import os.path
 
 Running = False
+
 crypto = CoinApi.getAllUSDTradeables()
 selected = [False]*len(crypto)
 ownedCryptos = []
+total = 0
+
 trader = -1
-x = [0]
-y = [0]
+t = [0]
+value = [0]
+timeSinceLast = 0
 
 def close():
     dpg.delete_item("pop")
 
 def receiveInputButton(sender, data): ##handle input from buttons and checkboxes
+    global Running
+    global selected
+    global timeSinceLast
+    global total
     pos = 0
     if sender == "Start":##Start
+        timeSinceLast = time.time()
+        total = dpg.get_value("total")
         try: ##if there are no cryptos selected, do not run
             pos = selected.index(True)
         except:
             pos = -1
-        if float(dpg.get_value("total")) >= 12.50 and pos != -1: ##at least one crypto and at least $12.50 has to be entered to run
-            if pos != -1:
-                if not os.path.exists("config.py"):
-                    #missConf()
-                    Running = True
-                    while True:
-                        renderGraph()
-                else:
-                    from config import account
-                    setLogger()
-                    trader = Trader(account['apikey'], account['secret'])
-                    #bot = CryptoBot(ownedCryptos, trader)
-                    ##bot.start()
-                    Running = True
-                    while True:
-                        renderGraph()
+        if total >= 12.50 and pos != -1: ##at least one crypto and at least $12.50 has to be entered to run
+            if not os.path.exists("config.py"):
+                #missConf()
+                Running = True
+                while Running:
+                    renderGraph()
+                ##kill all threads, sell, etc
+            else:
+                from config import account
+                setLogger()
+                trader = Trader(account['apikey'], account['secret'])
+                #bot = CryptoBot(ownedCryptos, trader)
+                ##bot.start()
+                Running = True
+                while Running:
+                    renderGraph()
         else:
             popup()
     elif sender == "Stop": ##Stop
         Running = False
     elif sender == "pick": ##color picker
-        print(data)
         dpg.configure_item("line", color = data)
+
+def setRunning(bool):
+    global Running
+    Running = bool
+
 def getTrader():
     return trader
 
 def receiveInput(sender, data): ##handle input from checkboxes and total money
+    global Running
     if sender == "total":
-        y.clear()
-        y.append(data)
-    else:
-        dpg.delete_item("Owned")
-        ownedCryptos.clear() ##we will be recreating this based on each selection, so deletion is enabled
-        selected[crypto.index(sender)] = data
-        for i in range(len(crypto)):
-            if selected[i] == True:
-                ownedCryptos.append(crypto[i])
-        selectedCryptos()
+        value.clear()
+        value.append(data)
+    else: ##checkboxes
+        if Running == False:
+            dpg.delete_item("Owned")
+            ownedCryptos.clear() ##we will be recreating this based on each selection, so deletion is enabled
+            selected[crypto.index(sender)] = data
+            for i in range(len(crypto)):
+                if selected[i] == True:
+                    ownedCryptos.append(crypto[i])
+            selectedCryptos()
+        else:
+            runningPop()
 
+def getRunning():
+    global Running
+    return Running
 
-def renderGraph(): ##rerender the graph with updated data every frame
-    dpg.delete_item("Plot")
-    graph()
-    dpg.render_dearpygui_frame() ##at some point this will have to be checking if stop has been clicked
-    time.sleep(10)
-
-def renderFrame(): ##stable renderer before clicking run
-    dpg.render_dearpygui_frame()
+def renderGraph(): ##rerender the graph with new data every 30 seconds
+    global timeSinceLast
+    global Running
+    resetGraph()
+    resetOptions()
+    resetCrypto()
+    while time.time() - timeSinceLast < 30:
+        dpg.render_dearpygui_frame() ##The last thing to do is poll for stop press
+    timeSinceLast = time.time()
+    
 
 def popup():
     dpg.add_window(label="WARNING", tag="pop", modal=True, on_close=close)
@@ -84,67 +108,101 @@ def popup():
     elif not ownedCryptos:
         dpg.add_text(parent="pop", default_value="You must select one or more cryptos")
 
+def runningPop():
+    dpg.add_window(label="WARNING", tag="pop", modal=True, on_close=close)
+    dpg.add_text(parent="pop", default_value="You cannot change settings after pressing start")
+
 def missConf():
     dpg.add_window(label="WARNING", tag="pop", modal=True, on_close=close)
     dpg.add_text(parent="pop", default_value="Missing config file, required to start trading")
 
 def selectedCryptos():
     with dpg.window(label="Selected Cryptos", height=1000, width = 220, pos=[600, 0], tag="Owned"): ##currently purchased cryptos
-        dpg.add_listbox(items=ownedCryptos, num_items=55, width=200)
+        dpg.add_listbox(items=ownedCryptos, num_items=55, width=200, tag="list")
 
 def tradableCryptos():
+    global Running
+    global selected
     with dpg.window(label="All Cryptos", width=220, height = 1000, pos=[820, 0], tag="Tradable"): ##checkbox for all cryptos
         for i in range(len(crypto)):
-            dpg.add_checkbox(label=crypto[i], tag=crypto[i], callback=receiveInput)
+            dpg.add_checkbox(label=crypto[i], default_value=selected[i], tag=crypto[i], callback=receiveInput)
 
 def graph():
-    x.append(x[len(x)-1]+10)
+    global t, value
+    t.append(t[len(t)-1]+30)
+    value.append(time.time())
     if getTrader() != -1:
-        y.append(time.time())
-        #y.append(Trader.getPortfolioUSDBalance(getTrader()))
+        pass
+        #value.append(Trader.getPortfolioUSDBalance(getTrader()))
     with dpg.window(label="Graph", width=600, height=400, tag="Plot"):
-        with dpg.plot(label="Overall performance", width=600, height=400):
-            dpg.add_plot_axis(dpg.mvXAxis, label = "time")
+        with dpg.plot(label="Overall performance", width=600, height=400, tag="perf"):
+            dpg.add_plot_axis(dpg.mvXAxis, label = "time (seconds)", tag="x_axis")
             dpg.add_plot_axis(dpg.mvYAxis, label="Value (USD)", tag ="y_axis")
-            dpg.add_line_series(x, y, parent="y_axis", tag = "line")
+            dpg.add_line_series(t, value, parent="y_axis", tag = "line")
 
 def options():
+    global Running
+    global total
     with dpg.window(label="Options", width=600, height=200, pos=[0, 400], tag="Options"): ##input window
-        dpg.add_input_float(label="Total Money", tag="total", default_value = 0, callback=receiveInput)
-        dpg.add_button(label="Start", tag="Start", callback=receiveInputButton)
-        dpg.add_button(label="Stop", tag = "Stop", callback=receiveInputButton)
+        if Running == False:
+            dpg.add_input_float(label="Total Money", tag="total", default_value = total, callback=receiveInput)
+            dpg.add_button(label="Start", tag="Start", callback=receiveInputButton)
+            dpg.add_button(label="Stop", tag = "Stop", callback=receiveInputButton)
+        else:
+            dpg.add_input_float(label="Total Money", tag="total", default_value = total, callback=runningPop)
+            dpg.add_button(label="Start", tag="Start", callback=runningPop)
+            dpg.add_button(label="Stop", tag = "Stop", callback=receiveInputButton)
 
 def colorPicker():
+    global Running
     with dpg.window(label="Graph Color", width=600, height=400, pos=[0, 600], tag="Picker"): ##color picker
-        dpg.add_color_picker(display_hex=True, tag="pick", callback=receiveInputButton)
+        if Running == False:
+            dpg.add_color_picker(display_hex=True, tag="pick", callback=receiveInputButton)
+        else:
+            dpg.add_color_picker(display_hex=True, tag="pick", callback=runningPop)
 
 
 def resetGraph():
+    dpg.delete_item("line")
+    dpg.delete_item("y_axis")
+    dpg.delete_item("x_axis")
+    dpg.delete_item("perf")
     dpg.delete_item("Plot")
     graph()
 
 def resetOptions():
+    dpg.delete_item("total")
+    dpg.delete_item("Start")
+    dpg.delete_item("Stop")
     dpg.delete_item("Options")
     options()
 
 def resetSelected():
+    global selected
+    dpg.delete_item("list")
     dpg.delete_item("Owned")
-    ownedCryptos.clear()
-    for i in range(len(selected)):
-        selected[i] = False
     selectedCryptos()
 
 def resetCrypto():
+    for i in crypto:
+        try:
+            dpg.delete_item(i)
+        except:
+            pass
     dpg.delete_item("Tradable")
     tradableCryptos()
-    print(ownedCryptos)
     resetSelected()
     dpg.render_dearpygui_frame()
 
 def resetPicker():
+    dpg.delete_item("pick")
     dpg.delete_item("Picker")
     colorPicker()
 
+def resetAll():
+    resetOptions()
+    resetSelected()
+    resetCrypto()
 
 def menuBar():
     dpg.add_window(label="menu", tag="window", no_close=True, no_title_bar=True, height = 300, width=300, pos = [1040, 0])
@@ -162,7 +220,6 @@ if __name__ == "__main__":
     dpg.create_viewport(title='CryptoBot', width=1940, height=1050) ##create outer window
     viewPos = [0, 0]
     dpg.set_viewport_pos(viewPos)
-
     #start creating windows and widgets
     selectedCryptos()
     graph()
@@ -174,7 +231,8 @@ if __name__ == "__main__":
     #finishing touches for dpg
     dpg.setup_dearpygui()
     dpg.show_viewport()
-    while Running == False:
-        renderFrame()
- 
+    while dpg.is_dearpygui_running():
+        dpg.render_dearpygui_frame()
+    Running = False
     dpg.destroy_context()
+    ##sell all crypto, kill all threads, maybe export the graph?
